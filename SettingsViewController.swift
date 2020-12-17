7ad976a485159c771623dbdb4d8f2ac6871b1aa0
zach dorame barajas <zachdoramebarajas@gmail.com>
2020-12-17 00:44:42 -0800

    set up variable retrieval for settings page
    - set the variables from stored variables on main page
    - update the 3-button thing for whatever values are stored
    - make it so that certain values are stored on first time installation

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -9,6 +9,31 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var presetOne: UITextField!
+    @IBOutlet weak var presetTwo: UITextField!
+    @IBOutlet weak var presetThree: UITextField!
+    
+    @IBAction func setFieldOne(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(presetOne.text, forKey: "presOne")
+        //print("here")
+        defaults.synchronize()
+        print("presetOne successfully set")
+    }
+    
+    @IBAction func setFieldTwo(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(presetTwo.text, forKey: "presTwo")
+        defaults.synchronize()
+        print("presetTwo successfully set")
+    }
+    
+    @IBAction func setFieldThree(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(presetThree.text, forKey: "presThree")
+        defaults.synchronize()
+        print("presetThree successfully set")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
